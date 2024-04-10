b92ce587a8e642f791a9a87e309c8b4d93e43483
Rassili <adnan.rassili47@gmail.com>
2024-04-10 17:50:00 +0000

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,8 @@
+import http from "../http"
+import { useState , useEffect } from "react";
+export default function home(){
+    const [expenses,setExpense]=useState([]);
+    return(
+        <div>Home page</div>
+    )
+}
